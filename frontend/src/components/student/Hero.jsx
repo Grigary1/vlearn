5ce8b2dcdd952c7dc56e90837a2df078759afc52
Grigary1 <grigaryvarkey16@gmail.com>
2025-03-05 19:48:21 +0530

    created homescreen and implemented authentication using clerk

---
+++ frontend/src/components/student/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+    </div>
+  )
+}
+
+export default Hero
---
+++ frontend/src/pages/student/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=''>Home
+    helllojljfsj
+    afdsl
+    fsnkj</div>
+  )
+}
+
+export default Home
